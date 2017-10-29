c40f35869d0d97ce6ed2cce098dad3ec3f4be991
Ruturaj123 <ruturaj2305@gmail.com>
2017-10-29 12:23:46 +0530

    Add Navigation Drawer

--- app/src/main/java/com/example/ruturaj/movies/MainActivity.java
+++ app/src/main/java/com/example/ruturaj/movies/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ruturaj.movies;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -24,11 +27,43 @@ public class MainActivity extends AppCompatActivity {
 
     private final static String api_key = "98429bdf1e5e077d0a3727fc98564a5b";
     private static String TAG = MainActivity.class.getSimpleName();
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.nav_drawer);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.setDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                switch (id){
+                    case R.id.menu_top_rated:
+                        Toast.makeText(getApplicationContext(), "Top rated", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.menu_now_playing:
+                        Toast.makeText(getApplicationContext(), "Now Playing", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.menu_upcoming:
+                        Toast.makeText(getApplicationContext(), "Upcoming", Toast.LENGTH_LONG).show();
+                        break;
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
 
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
@@ -58,6 +93,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item))
+            return true;
         return super.onOptionsItemSelected(item);
     }
+
 }
